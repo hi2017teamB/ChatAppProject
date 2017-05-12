b32eb83162096bc680e30a2a68549f5fc6b540f6
Reiki <periophthalmus24@gmail.com>
2017-05-12 10:38:50 +0900

    bot.pyで曜日取得

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
 import db
 import time
 from datetime import datetime
+from datetime import date
 import os
 
 BOT_ID = 5
 
-# def gomisute():
-
+def gomisute():
+    result = db.get_user_from_grade("b4")
+    text = "ゴミを捨てましょう"
+    for i in range(len(result)):
+        db.insert_message(result[i][0], BOT_ID, datetime.now().strftime('%Y%m%d%H%M%S'), text, 1)
+        print(result[i][0])
 
 if __name__ == '__main__':
     while True:
         print (datetime.now().strftime('%Y/%m/%d %H:%M:%S'));
-        result = db.get_user_from_grade("m1")
-        text = "ゴミを捨てましょう"
-        for i in range(len(result)):
-            db.insert_message(result[i][0], BOT_ID, datetime.now().strftime('%Y%m%d%H%M%S'), text, 1)
-            print(result[i][0])
+        print (datetime.now().weekday())
+
 
         time.sleep(1);

074d59657f194e0bcbe272a96bba5e95850b21d3
Reiki <periophthalmus24@gmail.com>
2017-05-17 18:24:43 +0900

    構文解析

--- bot.py
+++ bot.py
@@ -4,13 +4,14 @@ from datetime import datetime
 from datetime import date
 import os
 import re
+from syntax import Syntax
 
-def gomisute():
-    result = db.get_user_from_grade("b4")
-    text = "ゴミを捨てましょう"
-    for i in range(len(result)):
-        db.insert_message(result[i][0], BOT_ID, datetime.now().strftime('%Y%m%d%H%M%S'), text, 1)
-        print(result[i][0])
+# def gomisute():
+#     result = db.get_user_from_grade("b4")
+#     text = "ゴミを捨てましょう"
+#     for i in range(len(result)):
+#         db.insert_message(result[i][0], BOT_ID, datetime.now().strftime('%Y%m%d%H%M%S'), text, 1)
+#         print(result[i][0])
 
 def match_schedule(r):
     flag = False
@@ -52,51 +53,100 @@ def enter_schedule():
 # def find_time():
 
 def find_datetime(message):
-    print(message)
-    pattern = "^(?P<span>次の|毎(週|月))(?P<other>\S+)"
-    pattern2 = "^(([0-9]{4}年)?([1-9]|1[0-2])月)(([1-9]|[1-2][0-9]|3[0-1])日)(?P<other>\S+)"
-    match = re.search(pattern , message)
-    match2 = re.search(pattern2, message)
+    # pattern = "^(?P<span>次の?|毎(週|月)|来週の?|来月の?|来年の?)?(?P<days>([0-9]{1,2}月[0-9]{1,2}日|[0-9]{1,2}日|末|第[1-5][日月火水木金土]曜[日]?)+)の？(?P<time>(午前|午後)[0-9]{1,2}時([0-9]{1,2}分)?|[0-9]{1,2}:[0-9]{1,2})に?(?P<lab>研究室にいる|研究室の)?((?P<grade>B4|M1|M2)|(?P<team>awareness|AWARENESS|novel\s?interface|NOVEL\s?INTERFACE|cmc|CMC))に？(?P<item>\S+)をリマインド$"
+    pattern = "^(?P<span>次の?|毎(週|月)|来週の?|来月の?|来年の?)?(?P<days>([0-9]{1,2}月[0-9]{1,2}日|[0-9]{1,2}日|末|第[1-5][日月火水木金土]曜日?)+)の?(?P<time>(午前|午後)[0-9]{1,2}時([0-9]{1,2}分)?|[0-9]{1,2}:[0-9]{1,2})に?(?P<lab>研究室にいる|研究室の)?(?P<member>B4|M1|M2|awareness|AWARENESS|novel\s?interface|NOVEL\s?INTERFACE|cmc|CMC|全員|人)に?(?P<item>\S+)をリマインド$"
+    match = re.search(pattern, message)
+
     if match:
-        other = match.group("other")
-        if match.group("span") in {"次の", "毎月"}:
-            print(match.group("span"))
-            pattern = "^(?P<days>([0-9]{1,2}日|末|第[1-5][日月火水木金土]曜[日]?)+)に(?P<other>\S+)"
-            match = re.search(pattern , other)
-            if match:
-                other = match.group("other")
-                days = match.group("days")
-                print(match.group("days"))
-                pattern = "[0-9]{1,2}日|末|第[1-5][日月火水木金土]曜[日]?"
-                match_list = re.findall(pattern, days)
-                print(match_list)
-            else:
-                print("構文エラー")
-                return
+        print("match")
+        s = Syntax(match.group("span"), match.group("days"), match.group("time"), match.group("lab"), match.group("member"), match.group("item"))
+        return s
 
 
-        elif match.group("span") in {"毎週"}:
-            print(match.group("span"))
-            pattern = "(?P<days>([日月火水木金土]曜[日]?)+)(?P<other>\S+)"
-            match = re.search(pattern , other)
-            if match:
-                other = match.group("other")
-                days = match.group("days")
-                print(match.group("days"))
-                pattern = "[日月火水木金土]曜[日]?"
-                match_list = re.findall(pattern, days)
-                print(match_list)
-            else:
-                print("構文エラー")
-                return
-    elif match2:
-        other = match2.group("other")
     else:
-        print("構文エラー")
-        return
+        print("unmatch")
+        return False
 
-    pattern = "^(?P<time>\S*)に(?P<item>\S*)をリマインド$"
-    match = re.search(pattern, other)
+
+    # print(message)
+    # pattern = "^(?P<span>次の?|毎(週|月)|来週の?|来月の?|来年の?)(?P<other>\S+)"
+    # pattern2 = "^(([0-9]{4}年)?([1-9]|1[0-2])月)(([1-9]|[1-2][0-9]|3[0-1])日)(?P<other>\S+)"
+    # match = re.search(pattern , message)
+    # match2 = re.search(pattern2, message)
+    # if match:
+    #     other = match.group("other")
+    #     if match.group("span") in {"毎月"}:
+    #         print(match.group("span"))
+    #         pattern = "^(?P<days>([0-9]{1,2}日|末|第[1-5][日月火水木金土]曜[日]?)+)(?P<other>\S+)"
+    #         match = re.search(pattern , other)
+    #         if match:
+    #             other = match.group("other")
+    #             days = match.group("days")
+    #             print(match.group("days"))
+    #             pattern = "[0-9]{1,2}日|末|第[1-5][日月火水木金土]曜[日]?"
+    #             match_list = re.findall(pattern, days)
+    #             print(match_list)
+    #         else:
+    #             print("構文エラー")
+    #             return
+    #
+    #
+    #     elif match.group("span") in {"毎週"}:
+    #         print(match.group("span"))
+    #         pattern = "(?P<days>([日月火水木金土]曜[日]?)+)(?P<other>\S+)"
+    #         match = re.search(pattern , other)
+    #         if match:
+    #             other = match.group("other")
+    #             days = match.group("days")
+    #             print(match.group("days"))
+    #             pattern = "[日月火水木金土]曜[日]?"
+    #             match_list = re.findall(pattern, days)
+    #             print(match_list)
+    #         else:
+    #             print("構文エラー")
+    #             return
+    #
+    #     elif match.group("span") in {"次の", "次", "来週の", "来週"}:
+    #         print(match.group("span"))
+    #         pattern = "(?P<days>[日月火水木金土]曜[日]?)"
+    #
+    #     elif match.group("span") in {"来月の", "来月"}:
+    #         print(match.group("span"))
+    #         pattern = "(?P<days>[1-9]|[1-2][0-9]|3[0-1])日)"
+    #
+    #     elif match.group("span") in {"来年の", "来年"}:
+    #         print(match.group("span"))
+    #         pattern = "(?P<days>[日月火水木金土]曜[日]?)"
+    #
+    # elif match2:
+    #     other = match2.group("other")
+    # else:
+    #     print("構文エラー")
+    #     return
+    #
+    # pattern = "^の?(?P<time>\S*)に(?P<item>\S*)をリマインド$"
+    # match = re.search(pattern, other)
+    # if match:
+    #     print(match.group("time"))
+    #     print(match.group("item"))
+    #     time = match.group("time")
+    #     item = match.group("item")
+    #     pattern = "^[0-2]?[0-9]:[0-5][0-9]$"
+    #     pattern2 = "(午前|午後)[0-9]{1,2}時([0-9]{1,2}分|半)?"
+    #     match = re.search(pattern, time)
+    #     match2 = re.search(pattern2, time)
+    #     if match:
+    #         None
+    #     elif match2:
+    #         None
+    #     else:
+    #         print("構文エラー")
+    #         return
+    #
+    # else:
+    #     print("構文エラー")
+    #     return
+
 
     # pattern = "^毎月(?P<day>(([0-9]{1,2}日)|末|(第[1-5][日月火水木金土]曜[日]?))+)に(?P<item>\S*)をリマインド"
     # # pattern = "^毎月([0-9]{1,2}日)([1-9]{1,2}日)に(?P<item>\S*)をリマインド"
---
+++ syntax.py
@@ -0,0 +1,9 @@
+class Syntax:
+    def __init__(self, span, days, time, in_lab, grade, item):
+        self.item = item
+        self.span = span
+        self.days = days
+        self.time = time
+        self.in_lab = in_lab
+        self.grade = grade
+        self.item = item
